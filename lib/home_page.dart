9b280b8e0be16b685cbb480a08c1dc660aa4f9f3
Hira <hirashakeel866@gmail.com>
2026-05-17 23:45:17 -0700

    homepage

---
+++ lib/home_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager/login.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF263238),
+      appBar: AppBar(backgroundColor: Color(0xFF263238), toolbarHeight: -10),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 90),
+                    child: Image.asset(
+                      'assets/icons/bro.png',
+                      width: 60,
+                      height: 60,
+                    ),
+                  ),
+                  Container(
+                    // margin: EdgeInsets.only(left: 10, right: 10),
+                    child: Image.asset(
+                      'assets/images/app_img.png',
+                      width: 120,
+                      height: 120,
+                    ),
+                  ),
+                ],
+              ),
+              // SizedBox(height: 10),
+              Container(
+                margin: EdgeInsets.only(right: 20, left: 20),
+                color: Colors.white,
+                child: Image.asset('assets/images/pana.png'),
+              ),
+              SizedBox(height: 20),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.all(15),
+                child: Image.asset('assets/images/homepage1.png'),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return Login();
+                      },
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFFFED36A),
+                  minimumSize: Size(400, 60),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero,
+                  ),
+                ),
+                child: Text(
+                  "Let's Start",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+              // SizedBox(height: 10),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
